b6eccb5f04551e4b42995570cb5c2eb2161dc75c
Abishek0407 <thangavelabishek9@gmail.com>
2026-07-13 21:58:47 +0530

    Update bavked url

--- index.js
+++ index.js
@@ -14,8 +14,5 @@ mainserver.use("/api",require("./app"))
 mainserver.get("/check",(req,res)=>
     res.send("The server is Working"))
 
-const PORT = process.env.PORT || 5678;
-
-mainserver.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+mainserver.listen(5678, () =>
+    console.log("server is running"))
