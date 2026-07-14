d18a99af78d18333b85ae706ec0c6f74f606a7a6
Abishek0407 <thangavelabishek9@gmail.com>
2026-07-14 11:44:19 +0530

    Update bavked url

--- Db.js
+++ Db.js
@@ -1,11 +1,13 @@
-const mongoose =require("mongoose")
-require("dotenv").config()
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-mongoose.connect(process.env.MONGO_URI).then((req,res)=>{
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
     console.log("mongoose is working");
-    
-}).catch((err)=>{
-    console.log("the error is:",err);
-    
-})
-module.exports=mongoose
+}).catch((err) => {
+    console.log("the error is:", err);
+});
+
+module.exports = mongoose;
